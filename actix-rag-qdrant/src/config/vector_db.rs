3ff8253c83c9db815ad41e9381226422b1a1c6db
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-04-29 21:35:40 +0900

    Create method to delete collection as a development mode

--- actix-rag-qdrant/src/config/vector_db.rs
+++ actix-rag-qdrant/src/config/vector_db.rs
@@ -1,5 +1,8 @@
 use qdrant_client::client::QdrantClient;
 use std::sync::Arc;
+use log::info;
+use qdrant_client::prelude::{CreateCollection, Distance};
+use qdrant_client::qdrant::{VectorParams, vectors_config, VectorsConfig};
 use crate::config::app_config::AppConfig;
 
 #[derive(Clone)]
@@ -7,9 +10,34 @@ pub struct QdrantDb {
     pub client: Arc<QdrantClient>,
 }
 
-pub fn create_vector_client(setting: Arc<AppConfig>) -> QdrantClient {
+pub fn create_vector_client(setting: AppConfig) -> QdrantClient {
     let url = setting.vector_db.url.clone();
     QdrantClient::from_url(&url).build().unwrap()
+}
+
+pub async fn create_collection(qdrant_client: &Arc<QdrantDb>, collection_name: String) -> Result<(), ()> {
+    let result = qdrant_client.client.delete_collection(collection_name.clone()).await;
+    match result {
+        Ok(_) => {
+            info!("Deleted collection: {} successful", collection_name.clone());
+        }
+        Err(_) => {}
+    }
+    qdrant_client.client
+        .create_collection(&CreateCollection {
+            collection_name: collection_name.clone(),
+            vectors_config: Some(VectorsConfig {
+                config: Some(vectors_config::Config::Params(VectorParams {
+                    size: 4096,
+                    distance: Distance::Cosine.into(),
+                    ..Default::default()
+                })),
+            }),
+            ..Default::default()
+        })
+        .await
+        .unwrap();
+    Ok(())
 }
 
 impl QdrantDb {
